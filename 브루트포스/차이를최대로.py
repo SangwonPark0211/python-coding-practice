86d1cb8fac80cf5536bcf05b852b14c3295a9a02
PSW <tkddnjs0211@naver.com>
2020-02-26 22:10:29 +0900

    차이를최대로.py

---
+++ 브루트포스/차이를최대로.py
@@ -0,0 +1,63 @@
+def next_permutation(arr):
+
+    n = len(arr)-1
+    i = n
+    while arr[i-1] >= arr[i]:
+        i -= 1
+
+    j = i-1
+    diff = 10000
+    position_to_switch = 0
+
+    # select the number to chage position
+    for k in range(i, n+1):
+        if arr[k]-arr[j] > 0:
+            diff = min(diff, arr[k]-arr[j])
+    for m in range(i, n+1):
+        if arr[m]==arr[j]+diff:
+            position_to_switch = m
+            break
+
+    # change position of two number
+    temp = arr[j]
+    arr[j] = arr[position_to_switch]
+    arr[position_to_switch] = temp
+
+    temp_list = arr[j+1:]
+    temp_list.sort()
+    arr = arr[:j+1]
+    arr = arr + temp_list
+
+    return arr
+
+if __name__=='__main__':
+
+    num = int(input())
+    arr = []
+    arr = list(map(int, input().split()))
+    arr.sort()
+
+    # the number of permutation
+    per_num = 1
+    for i in range(num):
+        per_num *= (i+1)
+    per_num
+    per_num = per_num - 1
+
+    # calculate for the first list
+    sum_list = []
+    temp_sum = 0
+    for i in range(num-1):
+        temp_sum += abs(arr[i] - arr[i+1])
+    sum_list.append(temp_sum)
+    
+
+    for _ in range(per_num):
+        temp_sum = 0
+        arr = next_permutation(arr)
+        
+        for i in range(num-1):
+            temp_sum += abs(arr[i] - arr[i+1])
+        sum_list.append(temp_sum)
+
+    print(max(sum_list))
