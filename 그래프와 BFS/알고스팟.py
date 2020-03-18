43949a76c93c76b7f8c4184ebd97b0ea0b532f14
PSW <tkddnjs0211@naver.com>
2020-03-18 23:18:04 +0900

    Add 연결요소의갯수.py & 알고스팟.py

---
+++ 그래프와 BFS/알고스팟.py
@@ -0,0 +1,29 @@
+from queue import PriorityQueue as pq
+def dijkstra(): 
+    heap = pq() 
+    heap.put((0, (0, 0))) 
+    crush[0][0] = 0 
+    # 상하좌우
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1] 
+    while True:
+        cnt, (hi, hj) = heap.get() 
+        # print(cnt, hi, hj)
+        if hi == n-1 and hj == m-1:
+            break
+        for d in range(4):
+            x, y = hi + dx[d], hj + dy[d]
+            if 0<=x<n and 0<=y<m and crush[x][y]<0:
+                # crush[x][y] = (x,y)전까지 부순 벽의 갯수
+                crush[x][y] = cnt
+                if maze[x][y] == '1':
+                    # heap의 원소의 cnt = (x,y)에서 부순 벽의 갯수도 포함
+                    heap.put((cnt+1, (x, y)))
+                else:
+                    heap.put((cnt, (x, y)))
+                    
+m, n = map(int, input().split())
+maze = [list(input()) for _ in range(n)]
+crush = [[-1] * m for _ in range(n)]
+dijkstra()
+print(crush[n-1][m-1])
+
--- 그래프와 BFS/연결요소의갯수.py
+++ 그래프와 BFS/연결요소의갯수.py
@@ -1,39 +1,63 @@
-import sys
+# import sys
 
-N, M = 0, 0
-arr = []
-visit = []
+# N, M = 0, 0
+# arr = []
+# visit = []
+# cnt = 0
+
+# def dfs(x):
+
+#     if visit[x] == 1:
+#         return
+#     visit[x] = 1
+#     for i in range(N):
+#         if arr[x][i] == 1:
+#             dfs(i)
+
+
+# if __name__=='__main__':
+
+#     sys.setrecursionlimit(10**8)
+#     # N은 정점, M은 간선의 갯수
+#     N, M = map(int, sys.stdin.readline().split())
+#     arr = [[] for i in range(N)]
+#     visit = [0] * N
+
+#     for i in range(N):
+#         arr[i] = visit[:]
+
+#     for _ in range(M):
+#         u, v = map(int, sys.stdin.readline().split())
+#         arr[u-1][v-1] = 1
+#         arr[v-1][u-1] = 1
+
+#     for i in range(N):
+#         if visit[i] == 0:
+#             dfs(i)
+#             cnt += 1
+
+#     print(cnt)
+def dfs(v):
+    visited[v] = True
+    for e in adj[v]:
+        if not visited[e]:
+            dfs(e)
+            
+N, M = map(int, input().split())
+adj = [[] for i in range(N+1)]
+visited = [False] * (N + 1)
 cnt = 0
-
-def dfs(x):
-
-    if visit[x] == 1:
-        return
-    visit[x] = 1
-    for i in range(N):
-        if arr[x][i] == 1:
-            dfs(i)
-
-
-if __name__=='__main__':
-
-    sys.setrecursionlimit(10**8)
-    # N은 정점, M은 간선의 갯수
-    N, M = map(int, sys.stdin.readline().split())
-    arr = [[] for i in range(N)]
-    visit = [0] * N
-
-    for i in range(N):
-        arr[i] = visit[:]
-
-    for _ in range(M):
-        u, v = map(int, sys.stdin.readline().split())
-        arr[u-1][v-1] = 1
-        arr[v-1][u-1] = 1
-
-    for i in range(N):
-        if visit[i] == 0:
-            dfs(i)
-            cnt += 1
-
-    print(cnt)
+ 
+for i in range(M):
+    input_data = list(map(int, input().split()))
+    adj[input_data[0]].append(input_data[1])
+    adj[input_data[1]].append(input_data[0])
+    
+for i in range(1, len(visited)):
+    if not(visited[i]):
+        cnt += 1
+        dfs(i)
+        
+print(cnt)
+    
+        
