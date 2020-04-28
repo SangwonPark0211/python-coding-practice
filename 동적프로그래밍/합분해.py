a0f0ba231a653d6921587b0e6f4f0ed927451c2a
PSW <tkddnjs0211@naver.com>
2020-04-28 16:10:40 +0900

    합분해.py 추가

---
+++ 동적프로그래밍/합분해.py
@@ -0,0 +1,15 @@
+# 백준 2225
+
+n, k = list(map(int, input().split()))
+dp = [[0]*(n+1) for _ in range(k+1)]
+
+for i in range(n+1):
+    dp[1][i]=1
+for i in range(2, k+1):
+    for j in range(n+1):
+        if j==0:
+            dp[i][j]=1
+        else:
+            dp[i][j]=(dp[i][j-1]%1000000000+dp[i-1][j]%1000000000)%1000000000
+
+print(dp[k][n])
